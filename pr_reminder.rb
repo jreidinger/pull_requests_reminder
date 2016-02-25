5ce1635c03474c4ef7e4af06420b91c47a251181
Moises Deniz Aleman <mdeniz@suse.com>
2016-02-25 18:31:04 +0100

    Adapt the code to be more readable and more OO

--- pr_reminder.rb
+++ pr_reminder.rb
@@ -1,40 +1,54 @@
 #! /usr/bin/env ruby
+require 'open-uri'
+require 'json'
+require 'ostruct'
 
-require "open-uri"
-require "json"
-require "ostruct"
+# Represents an Organization in GitHub
+class Organization
+  attr_reader :name
 
-def usage
-<<-END
-  #{$0} <github organization> [repo-name-1] [repo-name-2] ...  [repo-name-n]
-  Tool to print report about pending pull request of given organization
-END
+  def initialize(name)
+    @name = name
+  end
+
+  # Retrieves all the repositories of the organization (with PRs by default)
+  def repositories_with_pull_requests(reponames = [])
+    repositories = []
+    page = 1
+    loop do
+      repositories_in_page = load_repos_in_page(page)
+      break if repositories_in_page.empty?
+      repositories.concat(repositories_in_page)
+      page += 1
+    end
+    repositories.select! { |repo| reponames.include?(repo.name) } unless reponames.empty?
+    repositories.select!(&:any_pull_requests?)
+    repositories
+  end
+
+  private
+
+  # Load repositories in the page from GitHub
+  def load_repos_in_page(page)
+    url = "https://api.github.com/orgs/#{name}/repos?page=#{page}&per_page=100"
+    open(url) do |json_file|
+      JSON.load(json_file.gets).map { |repo_data| Repository.new(repo_data) }
+    end
+  end
 end
 
-def print_usage
-  $stdout.puts usage
-end
-
-if ARGV.empty?
-  print_usage
-  exit 1
-end
-
+# Represents a Repository in GitHub
 class Repository < OpenStruct
-  def self.all(organization)
-    result = []
-    i = 1
-    cont = true
-    begin
-      tmp = load_part(organization, i)
-      if tmp.empty?
-        cont = false
-      else
-        result.concat tmp
-      end
-      i += 1
-    end while (cont)
-    return result
+  def pending_pull_requests
+    pull_requests = []
+    url = "https://api.github.com/repos/#{full_name}/pulls"
+    open(url) do |json_file|
+      pull_requests = JSON.load(json_file.gets).map { |pr_data| PullRequest.new(pr_data) }
+    end
+    pull_requests.select do |pull_request|
+      days = (Time.now.monday? || Time.now.tuesday?) ? 5 : 3 # do not count weekend in pending time
+      pull_request.pending_days > days
+    end
   end
 
   def any_pull_requests?
@@ -42,69 +56,46 @@ class Repository < OpenStruct
     # used only for quick decision if we need to inspect it
     open_issues_count > 0
   end
-private
-  def self.load_part(organization, part)
-    url = "https://api.github.com/orgs/#{organization}/repos?page=#{part}"
-    open(url) do |f|
-      repos = JSON.load(f.gets)
-      return repos.map{ |r| Repository.new(r) }
-    end
-  end
 end
 
+# Represents a PullRequest in GitHub
 class PullRequest < OpenStruct
-  def self.all(repository)
-    url = "https://api.github.com/repos/#{repository.full_name}/pulls"
-    open(url) do |f|
-      prs = JSON.load(f.gets)
-      return prs.map{ |p| PullRequest.new(p) }
-    end
-  end
-
   def updated
     @updated ||= Time.parse(updated_at)
   end
 
-  def pending_more_then?(days)
-    pending_days > days
-  end
-
   def pending_days
-    ((Time.now - updated)/24/3600).floor
+    ((Time.now - updated) / 24 / 3600).floor
   end
 end
 
-organization = ARGV[0]
-repos = Repository.all(organization)
-if ARGV.size > 1
-  reponames = ARGV[1, ARGV.size]
-  repos.select! { |repo| reponames.include?(repo.name) }
-end
-repos.select!(&:any_pull_requests?)
-
-result_message = ""
-repos.each do |repo|
-  begin
-    pull_requests = PullRequest.all(repo)
-  rescue OpenURI::HTTPError
-    result_message << "\n\n ERROR: API query limit exceeded"
-    break
-  end
-  pull_requests.select! do |pr|
-    # do not count weekend in pending time
-    days = (Time.now.monday? || Time.now.tuesday?) ? 5 : 3
-    pr.pending_more_then? days
-  end
-  next if pull_requests.empty?
-
-  msg = "\nPending requests in repository #{repo.name}:\n"
-  pull_requests.reduce(msg) do |msg, pr|
-    msg << "  - #{pr.title} (#{pr.pending_days} days)\n"
-    msg << "    #{pr.html_url}\n\n"
-  end
-
-  #visual splitter
-  result_message << msg
+def usage
+  <<-END
+    #{$PROGRAM_NAME} <github organization> [repo-name-1] [repo-name-2] ...  [repo-name-n]
+    Tool to print report about pending pull request of given organization repositories.
+    If a whitelist of repository names is passed then the tool will filter the results by them.
+  END
 end
 
-puts result_message
+if ARGV.empty?
+  puts usage
+  exit 1
+end
+
+organization, *reponames = ARGV
+message = ''
+begin
+  Organization.new(organization).repositories_with_pull_requests(reponames).each do |repository|
+    pending_pull_requests = repository.pending_pull_requests
+    next if pending_pull_requests.empty?
+
+    message << "\nPending requests in repository #{repository.name}:\n"
+    pending_pull_requests.each do |pull_request|
+      message << "  - #{pull_request.title} (#{pull_request.pending_days} days)\n"
+      message << "    #{pull_request.html_url}\n\n"
+    end
+  end
+rescue OpenURI::HTTPError
+  message << "\n\n ERROR: API query limit exceeded"
+end
+puts message
